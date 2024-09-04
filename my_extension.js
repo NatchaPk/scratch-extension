5c01f3df060518d331214ea95bb666f6c99f81f0
NatchaPk <natchapk963@gmail.com>
2024-09-04 08:07:21 +0700

    Add Scratch extension file

---
+++ my_extension.js
@@ -0,0 +1,13 @@
+fetch("https://script.google.com/macros/s/AKfycbyVpWdFnuZAKOwZDBziwgQ2f59bLzDLaOifbZxuHHj3/dev", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: "YourUsername",
+      score: 100
+    })
+  })
+  .then(response => response.text())
+  .then(data => console.log(data));
+  
